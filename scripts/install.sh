4fdf80e160b1f95f5dde36452fce3fef77fddb7d
isajediknight <luke@isajediknight.com>
2017-09-20 20:29:41 -0700

    Tested successful module installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,36 +5,46 @@ apt-mark showauto > temp.txt
 apt-mark showmanual >> temp.txt
 
 # Has curl been installed?  If not install it
+vim_installed=$(cat temp.txt | grep -w "vim")
+if [ "" == "$vim_installed" ]; then
+  sudo apt-get --force-yes --yes install vim
+else
+  echo "vim already installed"
+fi
+
+# Has curl been installed?  If not install it
 curl_installed=$(cat temp.txt | grep -w "curl")
 if [ "" == "$curl_installed" ]; then
   sudo apt-get --force-yes --yes install curl
+else
+  echo "curl already installed"
 fi
 
 # Has git been installed?  If not install it
 git_installed=$(cat temp.txt | grep -w "git")
 if [ "" == "$git_installed" ]; then
   sudo apt-get --force-yes --yes install git
+else
+  echo "git already installed"
 fi
 
 openssh_server_installed=$(cat temp.txt | grep -w "openssh-server")
 if [ "" == "$openssh_server_installed" ]; then
   sudo apt-get --force-yes --yes install openssh-server
+else
+  echo "openssh-server already installed"
 fi
 
 # Remove the temporary file we used to see what was installed
 rm -rf temp.txt
 
-# Recreate temp.txt so we can see if nvm is installed.
-# If it's not installed install it
-nvm > temp.txt
 
 # Has nvm been installed?  If not install it
-nvm_installed=$(cat temp.txt | grep -w "No command")
-if [ "" == "$nvm_instgalled" ]; then
+if [ ! -d "/home/$USER/.nvm" ]; then
   # If desired change the nvm version being installed from the URL below
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+  bash --login
+else
+  echo "nvm already installed"
 fi
-
-# Delete temporary file
-#rm -rf temp.txt
 
